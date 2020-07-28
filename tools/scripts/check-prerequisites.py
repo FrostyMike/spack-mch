04644c7dfd5dba6e2f1ef4707f473185548042bb
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-07-28 17:21:59 +0200

    Added helper script for checking spack prerequisites

---
+++ tools/scripts/check-prerequisites.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__      = "Mikhail Zhigun"
+__copyright__   = "Copyright 2020, MeteoSwiss"
+
+from sys import version_info as v
+from shutil import which
+
+assert v[0] == 3 and v[1] >= 5, 'Python should be 3.5 or older'
+
+def check_install(name):
+	assert which(name), '%s not installed' % name
+
+check_install("make")
+check_install("git")
+check_install("curl")
+check_install("gpg")
+check_install("make")
+
